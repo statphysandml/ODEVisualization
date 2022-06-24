260580522f95b12a3a29e1c9b76fb97e426c60c2
kades <kades@thphys.uni-heidelberg.de>
2022-06-24 08:58:34 +0200

    update

--- examples/flow_equations/NonlinearSystem/python_pybind/nonlinear_system.py
+++
@@ -1,7 +0,0 @@
-from odesolver.flow_equations import FlowEquations
-
-
-class NonlinearSystem(FlowEquations):
-    def __init__(self):
-        from nonlinearsystemsimulation import NonlinearSystemFlow, NonlinearSystemJacobians
-        super().__init__(flow=NonlinearSystemFlow(), jacobians=NonlinearSystemJacobians())
